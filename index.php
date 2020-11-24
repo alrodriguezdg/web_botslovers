ec5e17cb67c216c6f14342ea5a0629c6d08765d8
Al Rodriguez <alrodriguezdg@gmail.com>
2020-11-24 06:10:47 -0500

    Web botslovers Index

---
+++ index.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+	<meta charset="utf-8">
+	<title>botslovers Digital</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<meta name="description" content="Desarrollamos Software a la medida de las necesidades de su compañía.">
+	<!-- Favicon Web -->
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png">
+
+	<!-- General Css Styles -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link href="css/all.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/remodal.css">
+	<link rel="stylesheet" href="css/remodal-default-theme.css">
+	<link rel="stylesheet" type="text/css" href="css/slick.css">
+	<link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+
+<body>
+	<header>
+		<div class="wrapper main-header">
+			<div class="row container">
+				<div class="col col-md-3 col-sm-3 align-self-center">
+					<div class="logocont">
+						<div class="logostop">
+							<a href="https://botsloversdigital.com/" target="_blank">
+								<img src="images/logo-botslovers.png" alt="Botslovers" class="logoimg">
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="col col-sm-9 text-right align-self-center">
+					<nav>
+						<div id="menu-toggle" >
+							<i class="fas fa-bars"></i>
+						</div>
+						<ul id="menu-botslovers">
+							<li>
+								<a href="#">
+									¿Por que Botslovers?								
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Soluciones						
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Recursos
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Contacto
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									Login
+								</a>
+							</li>
+							<li>
+								<a class="btn-red" href="#">
+									PRIMEROS PASOS
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="wrapper">
+		<!--banner-->
+		<div class="banner-botslovers">
+			<div class="row container">
+				<div class="col-md-6 col-sm-12">
+					<h2>
+						<b>Somos <span>Botslovers</span></b> y nuestra misión es <b>¡Liberar a los humanos de tareas aburridas!</b>
+					</h2>
+					<p>
+						En Botslovers te ayudamos para crear en unos pocos pasos asistentes virtuales con inteligencia artificial.</p> 
+					<p>Te invitamos a conocer mas sobre nosotros. </p>
+					<a class="btn btn-outline-demo" href="#">
+						OBTENER DEMO
+					</a>
+					<a class="btn-red" href="#">
+						CONTACTARSE CON VENTAS
+					</a>
+				</div>
+				<div class="col-md-6 col-sm-12">
+					<img src="images/main-image-banner.png">
+				</div>	
+			</div>
+			<img class="divider-botslovers" src="images/divider-botslovers.png">		
+		</div>
+		<!--fin banner-->
+		<!--Canales-->
+		<div class="section-gray canals">
+			<div class="row container">
+				<div class="col-md-12 text-center">
+					<p>
+						Somos expertos en unir tecnologías para crear chatbots en gran variedad de dispositivos y en diferentes canales como Web, Facebook Messenger, WhatsApp y teléfono.
+					</p>
+					<img src="images/canales-plataformas-botslovers.png">
+				</div>
+			</div>
+		</div>
+		<!-- Fin Canales -->
+		<!--clientes-->
+		<div class="section-white customers">
+			<div class="row container">
+				<div class="col-md-12 text-center">
+					<h3>
+						<span>Más de 200 empresas</span> utilizan <span>Botslovers</span> para automatizar conversaciones y liberar a sus colaboradores de las tareas aburridas.
+					</h3>
+					<img src="images/clientes-botslovers.jpg">
+				</div>
+			</div>
+		</div>
+		<!-- Fin clientes-->
+		<!--demo-->
+		<div class="section-featured">
+			<div class="row container">
+				<div class="col-md-6 col-sm-12 align-self-center">
+					<h3>
+						¡Elige la plataforma mas fácil e intuitiva del mercado para crear tu Bot, empieza con tu demo ahora! 
+					</h3>
+				</div>
+				<div class="col-md-6 col-sm-12 align-self-center text-center">
+					<a class="btn btn-outline-demo" href="#">
+						OBTENER DEMO
+					</a>
+					<a class="btn-red" href="#">
+						CONTACTARSE CON VENTAS
+					</a>
+				</div>
+			</div>
+		</div>
+		<!-- Fin demo-->
+		<!--infografia-->
+		<div class="section-white infographic">
+			<div class="row container">
+				<div class="col-md-6 col-sm-12">
+					<h3>
+						<b>Ayuda y atiende a tus usuarios 24 x 7 por medio de las redes sociales y canales de comunicación</b> más conocidos a nivel mundial. Optimizando la operación con métricas y datos claves para tu negocio.
+					</h3>
+					<img class="phone-infographic" src="images/mockup-cel-botslovers-chatbot.png">
+				</div>
+				<div class="col-md-6 col-sm-12">
+					<img src="images/data-infographic.png">
+				</div>
+			</div>
+		</div>
+		<!-- Fin infografia-->
+		<!--Plataformas-->
+		<div class="section-gray platforms">
+			<div class="row container">
+				<div class="col-md-12 text-center">
+					<h2>Integra tus plataformas a Botslovers</h2>
+					<ul class="clientes-botslovers">
+						<li>
+							<img src="images/images-botslovers-logos-platforms-08.png">
+						</li>
+						<li>
+							<img src="images/images-botslovers-logos-platforms-09.png">
+						</li>
+						<li>
+							<img src="images/images-botslovers-logos-platforms-10.png">
+						</li>
+						<li>
+							<img src="images/images-botslovers-logos-platforms-11.png">
+						</li>
+						<li>
+							<img src="images/images-botslovers-logos-platforms-12.png">
+						</li>
+						<li>
+							<img src="images/images-botslovers-logos-platforms-13.png">
+						</li>
+						<li>
+							<img src="images/images-botslovers-logos-platforms-14.png">
+						</li>
+						<li>
+							<img src="images/images-botslovers-logos-platforms-15.png">
+						</li>
+						<li>
+							<img src="images/images-botslovers-logos-platforms-16.png">
+						</li>
+						<li>
+							<img src="images/images-botslovers-logos-platforms-17.png">
+						</li>
+						<li>
+							<img src="images/images-botslovers-logos-platforms-18.png">
+						</li>
+						<li>
+							<img src="images/images-botslovers-logos-platforms-19.png">
+						</li>
+						<li>
+							<img src="images/images-botslovers-logos-platforms-20.png">
+						</li>
+						<li>
+							<img src="images/images-botslovers-logos-platforms-21.png">
+						</li>
+						<li>
+							<img src="images/images-botslovers-logos-platforms-22.png">
+						</li>
+					</ul>
+					<hr>
+				</div>
+				<div class="col-md-6 col-sm-12 align-self-center">
+					<ul>
+						<li>
+							<b>Integra diversos CMS y CRM</b> con nuestra plataforma Chatbot.</li>
+						<li><b>Saca el máximo provecho a las interacciones</b> con tus usuarios y lleva tus KPIs y expectativas a otro nivel.</li>
+						<li>
+							<b>Nuestra plataforma cuenta con un sistema amigable</b> que hace fácil la integración y escalabilidad de requerimientos con servicios de terceros.
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-6 col-sm-12 align-self-center demo">
+					<a class="btn btn-outline-demo dark" href="#">
+						OBTENER DEMO
+					</a>
+					<br>
+					<a class="btn btn-red" href="#">
+						CONTACTARSE CON VENTAS
+					</a>
+				</div>
+			</div>
+		</div>
+		<!-- Fin Plataformas-->
+	</div>
+	<footer>
+		<div class="divider-gradient">
+		</div>
+		<div class="row container">
+			<div class="col-md-2 align-self-center logo-footer">
+				<a href="https://botsloversdigital.com/" target="_blank">
+					<img src="images/logo-botslovers-blanco.png" alt="Botslovers" class="logoimg">
+				</a>
+			</div>
+			<div class="col-md-10 align-self-center">
+				<ul>
+					<li>
+						<a href="#">Soluciones</a>
+					</li>
+					<li>
+						<a href="#">Enterprise</a>
+					</li>
+					<li>
+						<a href="#">Agencias</a>
+					</li>
+					<li>
+						<a href="#">Recursos</a>
+					</li>
+					<li>
+						<a href="#">Blog</a>
+					</li>
+					<li>
+						<a href="#">Eventos</a>
+					</li>
+					<li>
+						<a href="#">Centro de ayuda</a>
+					</li>
+					<li>
+						<a href="#">Inversión</a>
+					</li>
+					<li>
+						<a href="#">Contacto</a>
+					</li>
+					<li>
+						<a href="#">Nosotros</a>
+					</li>
+					<li>
+						<a href="#">Carrer</a>
+					</li>
+				</ul>
+			</div>
+			<hr>
+		</div>
+		<div class="row container">
+			<div class="col-md-6 col-sm-12">
+				<p>
+					LEGAL<br>POLITICA DE PRIVACIDAD - CONDICIONES DE SERVICIO
+				</p>
+			</div>
+			<div class="col-md-6 col-sm-12 slogan text-right">
+				<ul class="social-botslovers">
+					<li>
+						<a target="_blank" href="https://www.facebook.com/botslovers/"><img src="images/img-landing-botslovers-09.png"></a>
+					</li>
+					<li>
+						<a target="_blank" href="https://twitter.com/andrespulgarine?lang=es"><img src="images/img-landing-botslovers-10.png"></a>
+					</li>
+					<li>
+						<a target="_blank" href="https://www.linkedin.com/company/11169982/admin/"><img src="images/img-landing-botslovers-11.png"></a>
+					</li>
+					<li>
+						<a target="_blank" href="https://www.youtube.com/channel/UCWyIEYtnmhv1_9mFBxRdsDA"><img src="images/img-landing-botslovers-12.png"></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</footer>
+
+	<!-- Modal Cotización -->
+
+	<!-- LOADER SPECIAL-->		
+	<section id="loader_special">
+		<div class="cont_loader">
+			<div class="cart_loader">
+				<img src="images/elements/oval.svg" />
+				<p class="expecial_txt_loader">Cargando...</p>
+			</div>
+		</div>
+	</section>
+
+	<!-- Alertas -->
+	<div class="alerts_forms">
+		<div id="alertFailValidation" class="alertFail" >
+			<span> Por favor, ingrese todos los datos requeridos </span>
+		</div>
+		<div id="alertFailSend" class="alertFail" >
+			<span> Error al enviar mensaje, por favor intente más tarde  </span>
+		</div>
+		<div class="alertOk alertContacto" >
+			<span> Registro exitoso, pronto recibiras los detalles. ¡Gracias! </span>
+		</div>
+	</div>
+
+	<!-- General Scripts -->
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script src="js/slick.min.js"></script>
+	<!-- Contact Form -->
+	<script src="js/jquery.form.min.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Ajax  -->
+	<script type="text/JavaScript" src="js/ajax-woow.js"></script>
+	<!-- Remodal -->
+	<script src="js/remodal.js"></script>
+	<!-- FONT -->
+	<script src="js/all.min.js"></script>
+	<!--Parallax-->
+	<script src="js/parallax.min.js"></script>
+	<script type="text/javascript">
+		$( "#menu-toggle" ).click(function() {
+		  $( "#menu-botslovers" ).toggle( "slow" );
+		});
+		$(window).scroll(function() {    
+		    var scroll = $(window).scrollTop();
+
+		    if (scroll >= 50) {
+		        $(".main-header").addClass("menu-fixed");
+		    } else {
+		        $(".main-header").removeClass("menu-fixed");
+		    }
+		});
+	</script>
+
+</body>
+</html>
